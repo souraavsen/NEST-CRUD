f96261932c2baa18c06796a71848f9aedda58329
SOURAV SEN GUPTA <souravsen.gupta@geninvinci.invincix.com>
2023-08-15 15:34:00 +0600

    SQLite3 database connected and migration script added

--- nest-crud-api/ormconfig.ts
+++ nest-crud-api/ormconfig.ts
@@ -1,8 +1,16 @@
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
-export const config: SqliteConnectionOptions = {
+// interface ExtendedSqliteConnectionOptions extends SqliteConnectionOptions {
+//   cli: any;
+// }
+
+export const config: any = {
   type: 'sqlite',
-  database: 'nest-api-example',
-  entities: ['dist/src/**/*.entity.js'],
-  synchronize: true,
+  database: 'db',
+  entities: ['dist/src/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/**{.ts,.js}'],
+  cli: {
+    migrationsdir: 'src/db/migrations',
+  },
 };
--- nest-crud-api/src/app.module.ts
+++ nest-crud-api/src/app.module.ts
@@ -1,9 +1,10 @@
+import { config } from './../ormconfig';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({}), AuthModule, BookmarkModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, BookmarkModule],
 })
 export class AppModule {}
